e9b5ba0ccbe64309a6c4d6cff31dc65df968bd6a
dattn <daniel@duton.lu>
2016-01-09 18:25:04 +0100

    use smarty as composer package

--- classes/view.php
+++ classes/view.php
@@ -1,14 +1,12 @@
 <?php
 
-require_once Config::ROOT.'/libs/smarty3/Smarty.class.php';
-
 class View {
 
 	private static $_instance = null;
 	private $_smarty = null;
 
 	private function __construct() {
-		$this->_smarty = new Smarty();
+		$this->_smarty = new \Smarty();
 		$this->_smarty->setTemplateDir(Config::ROOT.'/templates');
 		$this->_smarty->setCompileDir(Config::ROOT.'/var/view/templates_c');
 		$this->_smarty->setCacheDir(Config::ROOT.'/var/view/cache');
