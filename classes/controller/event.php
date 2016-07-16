1856cd08cfbbfa0e22e25575d5482c11dcee15fc
Daniel Duton <daniel@duton.lu>
2016-07-16 22:59:10 +0200

    validate subscribe

--- classes/controller/event.php
+++ classes/controller/event.php
@@ -79,8 +79,16 @@ class Event extends \Controller {
                 INSERT
                 INTO tblfitness_user2event
                   (fiuser, fievent)
-                VALUES
-                  (:user, :event)
+                SELECT :user, :event
+                FROM tblfitness_event
+                WHERE (dtlimit IS NULL
+                        OR dtlimit > (
+                            SELECT COUNT(fiuser)
+                            FROM tblfitness_user2event
+                            WHERE fievent = idevent
+                        )
+                      )
+                  AND idevent = :event
             ', array(
                 'user' => $isAuth['user'],
                 'event' => $idevent
